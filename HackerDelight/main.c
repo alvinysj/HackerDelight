0bf5be9e41e6bdfa3186c01870202ffa3a92e243
alvinysj <alvinysj@gmail.com>
2016-10-12 21:29:28 +0800

    Initial Commit

---
+++ HackerDelight/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HackerDelight
+//
+//  Created by 杨思吉 on 2016/10/12.
+//  Copyright © 2016年 杨思吉. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
